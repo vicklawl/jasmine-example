a0ea2ae1ac9d84b0a1addab4f196f1b16ec3a0f9
Victor Lopez <victorlemur@gmail.com>
2018-03-13 19:18:29 -0400

    add isOver21 utility function

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -2,7 +2,23 @@ describe("utility functions", function()  {
   describe("fullName()", function()  {
     it("concatenates two name parts with a space", function()     {
       expect(fullName("Billy","Bob")).toEqual("Billy Bob");
-      
+
     });
   });
+
+  describe("isOver21()", ()=> {
+    it("returns true when the age is 21", () =>{
+      expect(isOver21("1997-01-01")).toBe(true);
+    });
+    it("returns true when the age is 21", () =>{
+      expect(isOver21("1992-01-01")).toBe(true);
+    });
+    it("returns true when the age is 21", () =>{
+      expect(isOver21("2000-01-01")).toBe(false);
+    });
+
+    // it("returns true when the age is over 21", ()=> {});
+    // it("returns false when the age under 21", ()=> {});
+  });
+
 } );
--- src/Utility.js
+++ src/Utility.js
@@ -2,3 +2,14 @@ function fullName(firstName, lastName) {
   return `${firstName} ${lastName}`;
 
 }
+function ageInYears(birthdayString) {
+  const birthday = new Date(birthdayString);
+  const ageInMilliseconds = Date.now() - birthday;
+  const millisecondsPerYear = 365.25 * 24 *60 * 60 * 1000;
+
+  return Math.floor(ageInMilliseconds/millisecondsPerYear);
+}
+
+function isOver21(birthdayString){
+  return ageInYears(birthdayString) >= 21;
+}
