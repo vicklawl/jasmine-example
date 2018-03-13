9d780f68c523120819dd97ccf4407cbc5f27ac91
Victor Lopez <victorlemur@gmail.com>
2018-03-13 19:35:20 -0400

    stub today's date in isOver21 utility function

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -7,13 +7,18 @@ describe("utility functions", function()  {
   });
 
   describe("isOver21()", ()=> {
+    const nowStub = new Date ("2018-03-13");
+
     it("returns true when the age is 21", () =>{
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("1997-01-01")).toBe(true);
     });
     it("returns true when the age is 21", () =>{
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("1992-01-01")).toBe(true);
     });
     it("returns true when the age is 21", () =>{
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("2000-01-01")).toBe(false);
     });
 
